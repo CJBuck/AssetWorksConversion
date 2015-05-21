abddcc21160a8ca172fd06c3c1023ff909cb3d08
DeathAndTaxes <davis.gerald@gmail.com>
2015-05-21 09:59:17 -0400

    Updated loading sql (was incorrectly saved copy of TargetPart)

--- Loading/Part/Copy TransformPartAdjustment to TargetPartAdjustment.sql
+++ Loading/Part/Copy TransformPartAdjustment to TargetPartAdjustment.sql
@@ -1,10 +1,4 @@
-INSERT INTO TargetPart
+INSERT INTO TransformPartAdjustment
 SELECT *
-FROM TransformPart
-WHERE
-	PartID IS NOT NULL
-	AND PartSuffix IS NOT NULL
-	AND Keyword IS NOT NULL
-	AND ShortDescription IS NOT NULL
-	AND ProductCategoryID IS NOT NULL
-	AND PartClassificationID IS NOT NULL
+FROM TransformPartAdjustment
+
