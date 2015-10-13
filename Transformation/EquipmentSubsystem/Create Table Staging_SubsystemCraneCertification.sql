d6ab0cb47e7432d6f84f76117363febfb5e5c8c2
Gerald Davis <davis.gerald@gmail.com>
2015-10-13 10:20:26 -0400

    Crane Certification dates not in WICM.  Per BA loading from provided spreadsheet

---
+++ Transformation/EquipmentSubsystem/Create Table Staging_SubsystemCraneCertification.sql
@@ -0,0 +1,18 @@
+USE [AssetWorksConversion]
+GO
+
+/****** Object:  Table [dbo].[Staging_SubsystemCraneCertification]    Script Date: 10/13/2015 10:18:35 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Staging_SubsystemCraneCertification](
+	[OBJECT_ID] [nvarchar](20) NOT NULL,
+	[CertificationDate] [nvarchar](100) NOT NULL
+) ON [PRIMARY]
+
+GO
+
+
