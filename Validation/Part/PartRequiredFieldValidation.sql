04205767adaa54ede7d76c1aba73a1148b67fb06
Christopher J. Buck <cbuck@marathonus.com>
2015-11-24 11:17:18 -0500

    Updated Part Required Field Validation SQL

--- Validation/Part/PartRequiredFieldValidation.sql
+++ Validation/Part/PartRequiredFieldValidation.sql
@@ -1,7 +1,12 @@
 SELECT
 	TP.PartID, TP.PartSuffix, TP.Keyword, TP.ShortDescription,
-	TP.ProductCategoryID, TP.PartClassificationID
+	TP.ProductCategoryID, TP.PartClassificationID,
+	-- WICM Fields
+	ph.MFG_NUMBER, ph.PART_NO, ph.PART_CAT, ph.[STATUS],
+	ph.RECTYPE, ph.PART_TYPE, ph.VENDOR_CD, ph.USAGE_CD,
+	ph.UNIT_ISSUE, ph.PART_GROUP
 FROM TransformPart TP
+	INNER JOIN SourceWicm220PartsHeader ph ON TP.PartID = ph.PART_NO
 WHERE
 	(ISNULL(TP.PartSuffix, 99) = 99)
 	OR (ISNULL(TP.Keyword, '') = '')
