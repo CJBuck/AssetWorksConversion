28c10a4fcc1b7d09eaaa39556959895b57339bc1
Christopher J. Buck <cbuck@marathonus.com>
2015-11-25 13:59:28 -0500

    Vendor Analysis

--- Transformation/PurchaseOrders/In Progress.sql
+++ Transformation/PurchaseOrders/In Progress.sql
@@ -1,1 +1,10 @@
 --
+select distinct POH.VENDORNUMBER, v.MUNISVendorID, v.MUNISVendorName
+from SourceWicm330POHeader POH
+	INNER JOIN TransformVendorWicmToMunisLookup v ON POH.VENDORNUMBER = v.WicmVendorNo
+	
+select *
+from SourceWicm330POHeader POH
+	inner join TransformVendor v on POH.VENDORNUMBER = v.VendorID
+	
+select * from TransformVendor
