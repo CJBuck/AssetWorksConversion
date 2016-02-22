b38f8742a3784a213eb98209239e330ac7346c11
Christopher J. Buck <cbuck@marathonus.com>
2016-02-22 14:47:55 -0500

    Transform Vendor Create Table Scripts

--- Transformation/Vendor/Create Table TransformVendor.sql
+++ Transformation/Vendor/Create Table TransformVendor.sql
@@ -1,15 +1,15 @@
 USE [AssetWorksConversion]
 GO
 
-/****** Object:  Table [dbo].[TransformVendor]    Script Date: 09/02/2015 08:16:10 ******/
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TransformVendor]') AND type in (N'U'))
+/****** Object:  Table [dbo].[TransformVendor]    Script Date: 02/17/2015 17:16:10 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TransformVendor]') AND TYPE IN (N'U'))
 DROP TABLE [dbo].[TransformVendor]
 GO
 
 USE [AssetWorksConversion]
 GO
 
-/****** Object:  Table [dbo].[TransformVendor]    Script Date: 09/02/2015 08:16:11 ******/
+/****** Object:  Table [dbo].[TransformVendor]    Script Date: 02/17/2015 17:16:10 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -22,27 +22,38 @@ GO
 CREATE TABLE [dbo].[TransformVendor](
 	[Control] [varchar](10) NOT NULL,
 	[VendorID] [varchar](15) NOT NULL,
-	[WICMVendorName] [varchar](100) NULL,
-	[MUNISVendorName] [varchar](100) NULL,
-	[MUNISVendorID] [varchar](25) NULL,
-	[VendorStatusID] [varchar](10) NULL,
+	[VendorName] [varchar](35) NOT NULL,
+	[ContactName] [varchar](35) NULL,
+	[AddressLine1] [varchar](30) NULL,
+	[AddressLine2] [varchar](30) NULL,
+	[AddressLine3] [varchar](30) NULL,
+	[AddressLine4] [varchar](30) NULL,
+	[Phone] [varchar](30) NULL,
+	[Fax] [varchar](30) NULL,
+	[EmailAddress] [varchar](200) NULL,
+	[SalesTaxRate] [decimal](22,4) NULL,
+	[FederalTaxID] [varchar](30) NULL,
+	[Active] [char](1) NOT NULL,
+	[MinorityOwned] [char](1) NULL,
+	[WomenOwned] [char](1) NULL,
+	[SmallBusiness] [char](1) NULL,
+	[VendorTypeID] [varchar](20) NULL,
+	[VendorStatusID] [varchar](20) NULL,
+	[VendorProvidesEquipment] [char](1) NULL,
+	[EquipmentAndComponentWarranties] [char](1) NULL,
+	[EquipmentRepairAndPM] [char](1) NULL,
+	[Parts] [char](1) NULL,
+	[PartsWarranties] [char](1) NULL,
+	[PartsRepairAndRebuild] [char](1) NULL,
+	[Fuel] [char](1) NULL,
+	[Testing] [char](1) NULL,
+	[CurrencyID] [char](1) NOT NULL,
+	[MinimumOrderValInVendorCurrency] [decimal](22,2),
+	[Contacts] [varchar](25) NOT NULL,
+	[SupportsAllLocations] [char](1) NULL,
+	[TrackEnterprisePurchasesReceiptsByStoreLocation] [char](1) NULL,
+	[StoreLocations] [varchar](25) NOT NULL,
 	[CreateDt] [datetime] NOT NULL
 ) ON [PRIMARY]
 
 GO
-
-SET ANSI_PADDING OFF
-GO
-
--- Copy data from staging to TransformVendor
-INSERT INTO TransformVendor
-(	[Control], VendorID, WICMVendorName, MUNISVendorName, MUNISVendorID, VendorStatusID, CreateDt	)
-SELECT 
-	'[i]',
-	LTRIM(RTRIM(stv.VendorID)),
-	LTRIM(RTRIM(stv.WICMVendorName)),
-	LTRIM(RTRIM(stv.MunisVendorName)),
-	LTRIM(RTRIM(stv.MunisVendorID)),
-	LTRIM(RTRIM(stv.[Vendor Status ID])),
-	GETDATE()
-FROM Staging_TransformVendor stv
---
+++ Transformation/Vendor/Create Table TransformVendorContact.sql
@@ -0,0 +1,41 @@
+USE [AssetWorksConversion]
+GO
+
+/****** Object:  Table [dbo].[TransformVendorContact]    Script Date: 02/18/2015 17:16:10 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[TransformVendorContact]') AND TYPE IN (N'U'))
+DROP TABLE [dbo].[TransformVendorContact]
+GO
+
+USE [AssetWorksConversion]
+GO
+
+/****** Object:  Table [dbo].[TransformVendorContact]    Script Date: 02/18/2015 17:16:10 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[TransformVendorContact](
+	[Control] [varchar](10) NOT NULL,
+	[VendorID] [varchar](15) NOT NULL,
+	[ContactTypeID] [varchar](20) NULL,
+	[LocationID] [varchar](10) NULL,
+	[StoreID] [varchar](20) NULL,
+	[ContactName] [varchar](35) NULL,
+	[Phone] [varchar](30) NULL,
+	[Fax] [varchar](30) NULL,
+	[Mobile] [varchar](30) NULL,
+	[AddressLine1] [varchar](30) NULL,
+	[AddressLine2] [varchar](30) NULL,
+	[AddressLine3] [varchar](30) NULL,
+	[AddressLine4] [varchar](30) NULL,
+	[EmailAddress] [varchar](200) NULL,
+	[Comments] [varchar](60) NULL,
+	[CreateDt] [datetime] NOT NULL
+) ON [PRIMARY]
+
+GO
